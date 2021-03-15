13a6a66e9091ebb3591298cd0fdce483a33fe5b9
Jerome Kim G. Belo <79246866+JKBelo15@users.noreply.github.com>
2021-03-15 21:37:27 +0800

    Uplaod and Test
    Added files for Upload and Test if it will replace existing file

--- Fahrenheit to Celsius/fahrenheit_to_celsius.c
+++ Fahrenheit to Celsius/fahrenheit_to_celsius.c
@@ -1,20 +1,29 @@
 #include <stdio.h>
 
+#define UPPER   300
+#define LOWER   0
+#define STEP    20
+
 int main() {
-    float fahr, cels;
-    int lower, upper, step;
+    // float fahr, cels;
+    // int lower, upper, step;
     
-    lower = 0;
-    upper = 300;
-    step = 20;
+    // lower = 0;
+    // upper = 300;
+    // step = 20;
 
-    fahr = lower;
+    // fahr = lower;
 
-    while(fahr <= upper) {
-        cels = (5.0*(fahr-32.0))/9.0;
-        printf("%3.0f %6.1f\n", fahr, cels);
-        fahr = fahr + step;
-    }
+    // while(fahr <= upper) {
+    //     cels = (5.0*(fahr-32.0))/9.0;
+    //     printf("%3.0f %6.1f\n", fahr, cels);
+    //     fahr = fahr + step;
+    // }
+
+    int fahr;
+
+    for (fahr = UPPER; fahr >= LOWER; fahr -= STEP)
+        printf("%3d %6.1f\n", fahr, (5.0/9.0)*(fahr-32));
 
     return 0;
 }
