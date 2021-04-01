dfee363c8479f0576e959734539a678e44a70740
Jerome Kim G. Belo <79246866+JKBelo15@users.noreply.github.com>
2021-04-01 21:53:23 +0800

    Finished Histogram Program

--- Histogram for Number of Words/histogram_words_number.c
+++ Histogram for Number of Words/histogram_words_number.c
@@ -3,28 +3,39 @@
 
 #define OUT 0
 #define IN  1
+#define ARRAY_SIZE 30
+#define MAX_INPUT 99
 
 /*  Program to count the number of words in the input and
     put it in a histogram (The C Programming Language - Exercise 1-13)*/
 
 void printArray (int ni, int *arr);
 void printHisto (int ni, int *arr);
-void printVertHisto (int ni, int *arr);
+void printVertHisto (int ni, int *arr, int max_input);
 
 int main() {
-    int c, nw, state, warr[10], ni;
+    int c, nw, state, warr[ARRAY_SIZE], ni;
 
     state = OUT;
     ni = 0;
 
     while ((c = getchar()) != EOF) {
         if (c == '\n') {
-            if (ni < 10) 
+            if (ni < ARRAY_SIZE)
                 warr[ni] = nw;
+            else if (ni >= ARRAY_SIZE) {
+                for (int i = 0; i <= ARRAY_SIZE; ++i)
+                    warr[i] = warr[i+1];
+                warr[ARRAY_SIZE-1] = nw;
+            }
             nw = 0;
             // printHisto(ni + 1, warr);
-            printVertHisto(ni + 1, warr);
+            printVertHisto(ni + 1, warr, ARRAY_SIZE);
             ++ni;
+            if (ni >= MAX_INPUT) {
+                printf("Max number of inputs reached, resetting to 0\n");
+                ni = 0;
+            }
         }
         if ( c == ' ' || c == '\n' || c == '\t') 
             state = OUT;
@@ -64,9 +75,13 @@ void printHisto(int ni, int * arr) {
     printf("\n");
 }
 
-void printVertHisto(int ni, int *arr) {
+void printVertHisto(int ni, int *arr, int max_input) {
     int max = 0;
     int *temp = arr;
+    int first_column = 1;
+    int origNI = ni;
+    if (ni > max_input)
+        ni = max_input;
     for (int i = 0; i < ni; ++i) {
         if (max < *arr)
             max = *arr;
@@ -74,17 +89,25 @@ void printVertHisto(int ni, int *arr) {
     }
     arr = temp;
     for (int i = max; i >= 0; --i) {
-        printf("%2d|", i);
+        printf("%5d|", i);
         for (int j = 0; j < ni; ++j) {
-            if (*arr >= i)
-                printf(" _");
+            if (i == 0)
+                printf("___");
+            else if (*arr >= i)
+                printf(" __");
             else 
-                printf("  ");
+                printf("   ");
             ++arr;
         }
-        arr = temp;
-        printf("\nInput 0");
         printf("\n");
+        arr = temp;
     }
+    printf("Input|");
+    if (origNI > max_input) {
+        first_column = origNI - max_input + 1;
+    }
+    for (int i = first_column; i <= origNI; ++i)
+        printf("%3d", i);
+    printf("\n\n\n");
     // printf("%d", max);
 }
